7f6e3ac0017d0bceee9ae8d1b936fa7c07e698c5
boemd <davide.boem@studenti.unipd.it>
2017-04-09 19:08:42 +0200

    Modificata Activity principale

--- app/src/main/java/it/unipd/dei/esp1617/h2o/InputActivity.java
+++ app/src/main/java/it/unipd/dei/esp1617/h2o/InputActivity.java
@@ -1,7 +1,14 @@
 package it.unipd.dei.esp1617.h2o;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.Spinner;
@@ -16,9 +23,13 @@ public class InputActivity extends AppCompatActivity
     private Spinner spinnerSex,spinnerAge;
     private RadioButton lessButton;
 
+    private String name;
+    private boolean lessnotif, male; //male = true, female = false;
+    private int age, weight;
+
+
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_input);
         spaceName=(EditText) findViewById(R.id.name_space);
@@ -29,5 +40,58 @@ public class InputActivity extends AppCompatActivity
         spaceSleep=(EditText) findViewById(R.id.sleep_time);
         spaceWake=(EditText) findViewById(R.id.wake_time);
         lessButton=(RadioButton) findViewById(R.id.less_notify);
+
+
+        ArrayAdapter<CharSequence> sex_adapter = ArrayAdapter.createFromResource(this, R.array.sex_array, android.R.layout.simple_spinner_item);
+        sex_adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerSex.setAdapter(sex_adapter);
+
+        ArrayAdapter<CharSequence> years_adapter = ArrayAdapter.createFromResource(this, R.array.years_array, android.R.layout.simple_spinner_item);
+        sex_adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerAge.setAdapter(years_adapter);
+
+        spinnerAge.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                age = position;
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                age = 0;
+            }
+        });
+
+        spinnerSex.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                male = (position==0)? false:true;
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                male = false;
+            }
+        });
+
+        spaceName.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                spaceName = null;
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                spaceName = (EditText) s;
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                spaceName = (EditText) s;
+            }
+        });
+
     }
+
+
 }
--- app/src/main/java/it/unipd/dei/esp1617/h2o/MainActivity.java
+++ app/src/main/java/it/unipd/dei/esp1617/h2o/MainActivity.java
@@ -1,13 +1,62 @@
 package it.unipd.dei.esp1617.h2o;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private Button bu;
+    private TextView tv1,tv2,tv3,tv4;
+    private FloatingActionButton faplus, faminus;
+    private int drunkGlasses =0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tv1 = (TextView) findViewById(R.id.textView1);
+        tv2 = (TextView) findViewById(R.id.textView2);
+        tv3 = (TextView) findViewById(R.id.textView3);
+        tv4 = (TextView) findViewById(R.id.textView4);
+        tv2.setText((CharSequence) bu);
+        tv4.setText((drunkGlasses>5)?"\"@string/c2\"":"\"@string/c1\"");
+
+        bu = (Button) findViewById(R.id.apri_second);
+        /**
+         * Passaggio all'activity per l'acquisizione delle informazioni sull'utente
+         */
+        bu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //start InputActivity
+                Intent intent = new Intent(MainActivity.this, InputActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        faplus = (FloatingActionButton)findViewById(R.id.plus_button);
+        faminus = (FloatingActionButton)findViewById(R.id.minus_button);
+        /**
+         * Incremento del numero di bicchieri bevuti dall'utente
+         */
+        faplus.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                drunkGlasses++;
+            }
+        });
+        /**
+         * Decremento del numero di bicchieri bevuti dall'utente
+         */
+        faminus.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                drunkGlasses--;
+            }
+        });
     }
 }
